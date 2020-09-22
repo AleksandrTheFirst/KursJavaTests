817f5cedbe92eb1bb2d1393e4edc485fb0b34dab
Александр Захаров <alexz1983@mail.ru>
2020-09-22 16:25:27 +0300

    Добавлен класс Point и его описание + метод

---
+++ sandbox/src/main/java/Program.java
@@ -0,0 +1,19 @@
+public class Program {
+
+    public static void main(String[] args) {
+
+
+
+
+    Point pointA = new Point(3, -4);
+    Point pointB = new Point(8, 7);
+    System.out.println("Расстояние между точкой A и точкой B равно " + distance(pointA, pointB));
+    System.out.println("Расстояние между точкой A и точкой B равно " + pointA.distance(pointB));
+
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+ }
