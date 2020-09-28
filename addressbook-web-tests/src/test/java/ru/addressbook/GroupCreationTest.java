9bb8eef5677d33e5d7c6f970d8e03fdbd07e42de
Aleksandr Zakharov <alexz1983@mail.ru>
2020-09-28 14:59:00 +0300

    Изменен мето setUp

--- addressbook-web-tests/src/test/java/ru/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/GroupCreationTest.java
@@ -16,6 +16,13 @@ public class GroupCreationTest {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//*[@value = 'Login']")).click();
     }
 
     @AfterMethod(alwaysRun = true)
@@ -25,13 +32,6 @@ public class GroupCreationTest {
 
     @Test
     public void createGroup() throws Exception{
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//*[@value = 'Login']")).click();
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
