c1793775dd4e0b69ec39d9d04d9033b81f7e9583
Aleksandr Zakharov <alexz1983@mail.ru>
2020-11-09 23:27:33 +0300

    Добавил чтение данных из файла Json для тестов создания контактов.

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,20 @@
 package ru.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.addressbook.model.ContactData;
 import ru.addressbook.model.Contacts;
+import ru.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,24 +23,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts(){
-        List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new ContactData().withFirstName("test1").withLastName("test1") });
-        list.add(new Object[] {new ContactData().withFirstName("test2").withLastName("test2") });
-        return list.iterator();
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line !=null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> group = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return group.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
     }
 
 
-    @Test(dataProvider = "validContacts")
+    @Test(dataProvider = "validContactsFromJson")
     public void createContact(ContactData contact) {
         app.goTo().contactsPage();
         Contacts before = app.contact().all();
-//        File photo = new File("src/test/resources/123.png");
-//        ContactData contact = new ContactData()
-//                .withFirstName(name)
-//                .withLastName(lastName)
-//                .withPhoto(photo);
-
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
